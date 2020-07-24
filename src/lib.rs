2df512b4dff5bbedc790862d7a7c27103141984e
Ercy Moreira Neto <ercy.neto@finnet.com.br>
2020-07-24 10:30:17 -0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, snake-rust!");
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+pub struct Vector {
+    pub x: f64,
+    pub y: f64
+}
+
+
+#[wasm_bindgen]
+impl Vector {
+    #[wasm_bindgen(constructor)]
+    pub fn new(x: f64, y: f64) -> Vector {
+        Vector {x, y}
+    }
+
+    pub fn subtract(&self, other: &Vector) -> Vector {
+        Vector::new(self.x - other.x, self.y - other.y);
+    }
+
+    pub fn scale_by(&self, number: f64) -> Vector {
+        Vector::new(self.x * number, self.y * number);
+    }
+}
+
+
+#[wasm_bindgen]
+pub struct Game {
+    pub width: i32,
+    pub height: i32,
+    pub speed: f64,
+    pub score: i32,
+    pub direction: Vector,
+    pub food: Vector,
+    snake: Vec<Vector>
+}
+
+#[wasm_bindgen]
+impl Game {
+    #[wasm_bindgen(constructor)]
+    pub fn new(width: i32, height: i32, speed: f64, snake_length: i32, direction: Vector) -> Game {
+            let head_x = (f64::from(width) / 2_f64).round() - 0.5;
+            let head_y = (f64::from(height) / 2_f64).round() - 0.5;
+            let head = Vector::new(head_x, head_y);
+            let tailtip = head.subtract(direction.scale_by(f64::from(snake_length)));
+            let snake = vec![tailtip,head];
+            //TODO
+            let food = Vector::new(0.5, 0.5);
+
+            Game {
+                width: width,
+                height: height,
+                speed: speed,
+                snake: snake,
+                direction: direction,
+                food: food
+            }
+    }
+}
